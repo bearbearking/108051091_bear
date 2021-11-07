92c79b155161016dce9f19da83b307a43cabd9fb
bearbeaeking <onmc521891103@gmail.com>
2021-11-07 15:41:35 +0800

    角色匯入

--- New Unity Project/Assets/2Dpackage/scripts/RubyMove.cs
+++ New Unity Project/Assets/2Dpackage/scripts/RubyMove.cs
@@ -13,6 +13,10 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+            Vector2 rubyMove = new Vector2();
+            rubyMove = transform.position;
+            rubyMove.x = rubyMove.x + 0.01f;
+            transform.position = rubyMove;
+
+        }
 }

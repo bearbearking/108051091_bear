75ffe130434033b524083c93c7fa04c5fd62828c
bearbeaeking <onmc521891103@gmail.com>
2021-11-20 22:50:29 +0800

    Ruby移動

--- New Unity Project/Assets/2Dpackage/scripts/RubyMove.cs
+++ New Unity Project/Assets/2Dpackage/scripts/RubyMove.cs
@@ -7,21 +7,25 @@ public class RubyMove : MonoBehaviour
 
     public float moveSpeed;
 
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 rubyPosition = transform.position;
+
         rubyPosition.x = rubyPosition.x + moveSpeed * Input.GetAxis("Horizontal")  ;
         //print(Input.GetAxis("Horizontal"));
         transform.position = rubyPosition;
         rubyPosition.y = rubyPosition.y + moveSpeed * Input.GetAxis("Vertical");
         //print(Input.GetAxis("Horizontal"));
         transform.position = rubyPosition;
+        rb.MovePosition(rubyPosition);
     }
 }

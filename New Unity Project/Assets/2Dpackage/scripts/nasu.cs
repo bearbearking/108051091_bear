da57de5652ff23fc8248a0bfd8178b12f7d6e0a1
bearbearking <90890397+bearbearking@users.noreply.github.com>
2021-12-14 14:09:41 +0800

    Update

--- New Unity Project/Assets/2Dpackage/scripts/nasu.cs
+++ New Unity Project/Assets/2Dpackage/scripts/nasu.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 
 public class nasu : MonoBehaviour
 {
+    public GameObject pickE;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Instantiate(pickE, gameObject.transform.position, Quaternion.identity);
+
         RubyMove RubyRunDown1 = collision.GetComponent<RubyMove>();
         print("碰到的東西是:" + RubyRunDown1);
         RubyRunDown1.ChangeHealth(1);
         Destroy(gameObject);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
